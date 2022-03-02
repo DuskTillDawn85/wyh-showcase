b1be8aeaa279097ff706e0f39373171b6fce3831
shawn <1067608727@qq.com>
2022-03-02 22:34:16 +0800

    update config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/wyh-showcase" : "/",
+  publicPath:"/wyh-showcase/",
 })
